7d20c8fc9939ef9f00ea3d273099aa9e7d56fb98
luochensama <rCore-lab1>
2021-10-16 23:50:21 +0800

    构建裸机执行环境

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,7 +1,16 @@
-// os/src/lang_items.rs
+#![feature(panic_info_message)]
+
 use core::panic::PanicInfo;
+use crate::shutdown;
+use crate::println;
+use crate::print;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!("Panicked at {}:{} {}", location.file(), location.line(), info.message().unwrap());
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 #![no_std]
 #![no_main]
 #![feature(llvm_asm)]
+#![feature(global_asm)]
+#![feature(panic_info_message)]
 
 use core::fmt;
 use core::fmt::Write;
+use crate::sbi::sbi_call;
 
+#[macro_use]
 mod lang_items;
+mod sbi;
 
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_WRITE: usize = 64;
@@ -33,9 +38,16 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
 
 struct Stdout;
 
+pub fn console_putchar(c: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
+}
+
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
+        //sys_write(STDOUT, s.as_bytes());
+        for c in s.chars() {
+            console_putchar(c as usize);
+        }
         Ok(())
     }
 }
@@ -58,8 +70,29 @@ macro_rules! println {
     }
 }
 
+const SBI_SHUTDOWN: usize = 8;
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+
+pub fn shutdown() -> ! {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    panic!("It should shutdown!");
+}
+
+global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
 #[no_mangle]
-extern "C" fn _start() {
+pub fn rust_main(){
     println!("Hello, world!");
-    sys_exit(9);
+    panic!("It should shutdown!");
+    shutdown();
 }
